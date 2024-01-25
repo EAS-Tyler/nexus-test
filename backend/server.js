5203f37dd4c449442af5f826c1be5b15426bdf4b
EAS-Tyler <tyler@enterpriseautomation.co.uk>
2024-01-25 15:29:55 +0000

    dockerfile update

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,14 @@ const { app } = require("./app")
 const port = 4000
 
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next()
+})
+
+
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
 })
