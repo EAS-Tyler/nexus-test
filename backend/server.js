e302685e9700b71d9d75fe2537b5e189bbcda6ee
EAS-Tyler <tyler@enterpriseautomation.co.uk>
2024-01-25 15:31:53 +0000

    dockerfile update

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,8 @@ const port = 4000
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*")
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    // res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    // res.header('Access-Control-Allow-Headers', 'Content-Type');
     next()
 })
 
